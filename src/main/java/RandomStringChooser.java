d841be78a751669a57aa7d273b7f7856162efb11
AdenChiu22 <142437577+AdenChiu22@users.noreply.github.com>
2024-05-27 18:12:51 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,22 @@
 import java.util.*;
 public class RandomStringChooser
+public class RandomStringChooser
 {
   /* to be implemented in part (a) */
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
+  private ArrayList<String> wordList;
+  public RandomStringChooser(String[] word) {
+    wordList = new ArrayList<String>();
+    for (String singleWord: word) {
+      wordList.add(singleWord);
+    }
+  }
+  public String getNext() {
+    if (wordList.size() > 0) {
+      return wordList.remove((int) (Math.random() * wordList.size()));
+    }
+    return "NONE";
+  }
 }
